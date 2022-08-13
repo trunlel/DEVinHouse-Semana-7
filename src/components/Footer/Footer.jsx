cf1063a5fc11f2bf3a6ca708870555aa58079d6a
Darlan Martins <langodarlan46@gmail.com>
2022-08-13 17:37:38 -0300

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
 import "./footer.css";
+import { BsFacebook } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 
 export const Footer = () => {
   return (
-    <div className="footer">
-      <p>Bob Lanches</p>
-    </div>
+    <footer>
+      <div className="footer">
+        <p className="copy">© Bob Lanches LTDA.</p>
+        <div className="social">
+          <i className="icons">
+            <BsFacebook />
+          </i>
+          <i className="icons">
+            <BsWhatsapp />
+          </i>
+          <i className="icons">
+            <BsInstagram />
+          </i>
+        </div>
+
+        <p className="endereco">
+          Palhoça, Santa Catarina
+          <hr />
+          190 R. Nossa Sra. dos Navegantes
+          <hr />
+          (48) 9 9801-5805
+          <br />
+        </p>
+      </div>
+    </footer>
   );
 };
